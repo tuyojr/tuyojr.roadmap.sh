6892f9ac58e69ea7932e333c162c11f9d0ff0869
tuyojr <olutuyod@gmail.com>
2026-02-03 13:58:50 +0100

    removed multiple echo lines

--- log-archive-tool/log-archive-tool.sh
+++ log-archive-tool/log-archive-tool.sh
@@ -8,41 +8,28 @@ logs_archive=logs_archive_"$log_date".tar.gz
 dir_to_archive_check ()
 {
     if [[ -z $1 ]]; then
-        echo
-        echo "This script requires a log directory argument."
-        echo "Usage: ./<script> ./path/to/output"
-        echo
+        echo -e "\nThis script requires a log directory argument."
+        echo -e "Usage: ./<script> ./path/to/output\n"
         exit 1
     else
-        echo
-        echo "Log directory value set: $dir_to_archive"
-        echo
+        echo -e "\nLog directory value set: $dir_to_archive\n"
     fi
 }
 
 archive ()
-{   
+{
     if [[ -z $(ls -l $log_dir) ]]; then
-        echo
-        echo Log directory not available, creating now...
-        echo
+        echo -e "\nLog directory not available, creating now...\n"
         mkdir $log_dir
         ls -l
-        echo
-        echo Log directory created.
-        echo
+        echo -e "\nLog directory created."
     else
-        echo
-        echo Log directory available
+        echo -e "\nLog directory available"
         ls -l
-        echo
     fi
-    echo
-    echo "Creating logs $log_dir$logs_archive"
-    echo
+    echo -e "\n\nCreating logs $log_dir$logs_archive\n"
     tar -cvvf "$log_dir$logs_archive" "$dir_to_archive"
-    echo
-    echo Done.
+    echo -e "\nDone."
 }
 
 main ()
